fa4d54e97a15df49d0fbfaab22618fa2035f2bc7
BenWhite101 <ben.white101@hotmail.co.uk>
2021-06-09 20:34:21 +0100

    header

---
+++ src/pages/Header.js
@@ -0,0 +1,12 @@
+// Step 1: Import React
+import * as React from 'react'
+// Step 2: Define your component
+const Header = () => {
+  return (
+    <header>
+        <a href="/">Ben White</a>
+    </header>
+  )
+}
+// Step 3: Export your component
+export default Header
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,9 @@ import * as React from 'react'
 // Step 2: Define your component
 const IndexPage = () => {
   return (
+    
     <main>
+        <Header />
       <title>Home Page</title>
       <h1>Welcome to my Gatsby site!</h1>
       <p>I'm making this by following the Gatsby Tutorial.</p>
